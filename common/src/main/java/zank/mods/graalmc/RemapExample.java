6cae217d14c9b0cd57920f4273b925e4dc4b2ed6
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-04-04 16:04:26 +0800

    add example

--- common/src/main/java/zank/mods/graalmc/RemapExample.java
+++ common/src/main/java/zank/mods/graalmc/RemapExample.java
@@ -1,5 +1,7 @@
 package zank.mods.graalmc;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
 import zank.mods.graalmc.api.MemberRemapper;
 
 import java.lang.reflect.Field;
@@ -13,18 +15,39 @@ public class RemapExample implements MemberRemapper {
     public final int graal$field = 42;
 
     public String graal$method() {
-        return "wow";
+        return """
+            I dont know if anyone actually bother to download source jar of GraalMC
+            So let me just provide an example here""";
+    }
+
+    private static void example() {
+        MemberRemapper.GLOBAL.set(new RemapExample());
+        var access = HostAccess.newBuilder().allowPublicAccess(true).build();
+        try (var context = Context.newBuilder("js")
+            .allowHostClassLookup("zank.mods.graalmc.RemapExample"::equals)
+            .allowHostAccess(access)
+            .build()
+        ) {
+            context.eval(
+                "js", """
+                    let RemapExample = Java.type("zank.mods.graalmc.RemapExample")
+                    let example = new RemapExample()
+                    console.log(example.method())
+                    console.log(example.field)
+                    """
+            );
+        }
     }
 
     @Override
     public String remapField(Field field) {
         var name = field.getName();
-        return name.startsWith("graal$")? name.substring("graal$".length()) : name;
+        return name.startsWith("graal$") ? name.substring("graal$".length()) : name;
     }
 
     @Override
     public String remapMethod(Method method) {
         var name = method.getName();
-        return name.startsWith("graal$")? name.substring("graal$".length()) : name;
+        return name.startsWith("graal$") ? name.substring("graal$".length()) : name;
     }
 }
