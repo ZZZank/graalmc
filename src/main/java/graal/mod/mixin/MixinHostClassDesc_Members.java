45753a86947dfb7a1128e46aa31fff25061bca1c
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-04-10 21:18:07 +0800

    fix: MemberRemapper not working

--- src/main/java/graal/mod/mixin/MixinHostClassDesc_Members.java
+++ src/main/java/graal/mod/mixin/MixinHostClassDesc_Members.java
@@ -1,7 +1,6 @@
 package graal.mod.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import graal.mod.api.MemberRemapper;
@@ -12,8 +11,7 @@ import java.lang.reflect.Method;
 /**
  * @author ZZZank
  */
-@Mixin(targets = "graal.oracle.truffle.host.HostClassDesc$Members", remap = false)
-@Pseudo
+@Mixin(targets = "com.oracle.truffle.host.HostClassDesc$Members", remap = false)
 public abstract class MixinHostClassDesc_Members {
 
     @Redirect(method = "collectPublicFields", at = @At(value = "INVOKE", target = "Ljava/lang/reflect/Field;getName()Ljava/lang/String;"))
